a5755a92a629eeb2fddd247bae3f279a83b5382b
velicanakcakaya <v.akcakaya@hanntech.de>
2022-12-03 19:32:00 +0100

    Final refactors.

--- src/day3/mod.rs
+++ src/day3/mod.rs
@@ -1,3 +1,7 @@
+use std::iter::Zip;
+use std::str::Chars;
+use std::vec;
+
 const LETTERS_LOWERCASE: [char; 26] = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
     't', 'u', 'v', 'w', 'x', 'y', 'z',
@@ -26,6 +30,14 @@ impl<T> ToVec<T> for (T, (T, T)) {
     }
 }
 
+fn get_chars<'a>(group: &[&'a str]) -> Zip<Chars<'a>, Zip<Chars<'a>, Chars<'a>>> {
+    group[0].chars().zip(
+        group[1]
+            .chars()
+            .zip(group.get(2).unwrap_or(&group[0]).chars()),
+    )
+}
+
 fn get_priority(letter: &str) -> u8 {
     let letter = letter.chars().next().unwrap();
     let find_position = |set: [char; 26]| set.into_iter().position(|char| char == letter);
@@ -37,24 +49,21 @@ fn get_priority(letter: &str) -> u8 {
     } as u8
 }
 
-fn prepare_input_part_1(input: &str) -> Vec<[&str; 2]> {
+fn prepare_input_part_1(input: &str) -> Vec<Vec<&str>> {
     input
         .trim()
         .split('\n')
-        .map(|line| {
-            let (first_compartment, second_compartment) = line.split_at(line.len() / 2);
-            [first_compartment, second_compartment]
-        })
+        .map(|line| line.split_at(line.len() / 2).to_vec())
         .collect()
 }
 
-fn prepare_input_part_2(input: &str) -> Vec<[&str; 3]> {
+fn prepare_input_part_2(input: &str) -> Vec<Vec<&str>> {
     input
         .trim()
         .split('\n')
         .collect::<Vec<_>>()
         .chunks(3)
-        .map(|group| [group[0], group[1], group[2]])
+        .map(<[_]>::to_vec)
         .collect()
 }
 
@@ -68,63 +77,53 @@ fn compartment_has_char(compartment: &[&str], char: char) -> Option<char> {
     None
 }
 
-fn get_common_item_part_1(compartment: [&str; 2]) -> Option<char> {
-    compartment[0]
-        .chars()
-        .zip(compartment[1].chars())
-        .find_map(|chars| {
-            chars
-                .to_vec()
-                .into_iter()
-                .find_map(|c| compartment_has_char(&compartment, c))
-        })
+fn get_common_item(compartment: Vec<&str>) -> Option<char> {
+    get_chars(&compartment).find_map(|chars| {
+        chars
+            .to_vec()
+            .into_iter()
+            .find_map(|c| compartment_has_char(&compartment, c))
+    })
 }
 
-fn get_common_item_part_2(group: [&str; 3]) -> Option<char> {
-    group[0]
-        .chars()
-        .zip(group[1].chars().zip(group[2].chars()))
-        .find_map(|chars| {
-            chars
-                .to_vec()
-                .into_iter()
-                .find_map(|c| compartment_has_char(&group, c))
-        })
-}
-
-fn sum_priorities<T>(groups: Vec<T>, get_char_predicate: fn(T) -> Option<char>) -> u32 {
+fn sum_priorities(
+    groups: Vec<Vec<&str>>,
+    get_char_predicate: fn(Vec<&str>) -> Option<char>,
+) -> u32 {
     groups.into_iter().fold(0, |acc, compartment| {
         acc + get_priority(&get_char_predicate(compartment).unwrap().to_string()) as u32
     })
 }
 
-pub fn part_1(groups: Vec<[&str; 2]>) -> u32 {
-    sum_priorities(groups, get_common_item_part_1)
+pub fn execute(groups: Vec<Vec<&str>>) -> u32 {
+    sum_priorities(groups, get_common_item)
 }
 
-pub fn part_2(groups: Vec<[&str; 3]>) -> u32 {
-    sum_priorities(groups, get_common_item_part_2)
+pub fn part_1(groups: Vec<Vec<&str>>) -> u32 {
+    execute(prepare_input_part_1(include_str!("input.txt")))
+}
+
+pub fn part_2(groups: Vec<Vec<&str>>) -> u32 {
+    execute(prepare_input_part_2(include_str!("input.txt")))
 }
 
 #[cfg(test)]
 mod test {
-    use crate::day3::{
-        get_common_item_part_1, part_1, part_2, prepare_input_part_1, prepare_input_part_2,
-    };
+    use crate::day3::{execute, prepare_input_part_1, prepare_input_part_2};
 
     const EXAMPLE_INPUT: &str = "vJrwpWtwJgWrhcsFMMfFFhFp\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\nPmmdzqPrVvPwwTWBwg\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\nttgJtRGJQctTZtZT\nCrZsJsPPZsGzwwsLwLmpwMDw";
     const FILE_INPUT: &str = include_str!("input.txt");
 
     #[test]
     fn test_part_1() {
-        assert_eq!(part_1(prepare_input_part_1(EXAMPLE_INPUT)), 157);
-        assert_eq!(part_1(prepare_input_part_1(FILE_INPUT)), 7553);
+        assert_eq!(execute(prepare_input_part_1(EXAMPLE_INPUT)), 157);
+        assert_eq!(execute(prepare_input_part_1(FILE_INPUT)), 7553);
     }
 
     #[test]
     fn test_part_2() {
-        assert_eq!(part_2(prepare_input_part_2(EXAMPLE_INPUT)), 70);
-        assert_eq!(part_2(prepare_input_part_2(FILE_INPUT)), 2758);
+        assert_eq!(execute(prepare_input_part_2(EXAMPLE_INPUT)), 70);
+        assert_eq!(execute(prepare_input_part_2(FILE_INPUT)), 2758);
     }
 
     #[test]
